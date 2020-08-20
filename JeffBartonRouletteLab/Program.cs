d13063f8a2414b7928348a591dd37d5f1018a5d6
Jeffrey Barton <jeffrey.barton99@outlook.com>
2020-08-20 06:40:49 -0500

    Initial Visual Studio Commit

---
+++ JeffBartonRouletteLab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JeffBartonRouletteLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Kitty!");
+        }
+    }
+}
